29da645ba710c87af48e9532267ab2d623c9c324
Michael Hahn <mhahn29@gmail.com>
2019-07-28 21:36:19 -0700

    code

--- stimuli/safavi_etal_2016_persian/reverseWords1.py
+++ stimuli/safavi_etal_2016_persian/reverseWords1.py
@@ -1,8 +1,9 @@
 with open("items_expt1.txt", "r") as inFile:
-   for line in inFile:
+   for linenum, line in enumerate(inFile):
       line = line.strip()
       if len(line) < 3:
          print(line)
       else:
          line = line[1:-1].split(" ")[::-1]
-         print((" ".join(line)).replace("_", " "))
+         if True or linenum%5 in [2,4]:
+            print((" ".join(line)).replace("_", " "))
