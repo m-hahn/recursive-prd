3db0156edaa5809c1224e11684046569deabd753
Michael Hahn <mhahn29@gmail.com>
2019-11-28 15:25:19 -0800

    code

--- autoencoderTabulateResultsByRateWeight.py
+++ autoencoderTabulateResultsByRateWeight.py
@@ -15,8 +15,12 @@ for filen in logs:
    params = params.replace("Namespace(", "")[:-1].split(", ")
    params = [x for x in params if x.split("=")[0] in ["RATE_WEIGHT", "batchSize", "entropy_weight", "learning_rate", "momentum"]]
    memRate = memRate.replace("tensor(", "").replace(", device='cuda:0', grad_fn=<MeanBackward0>)", "")
-   results.append((round(float(perform),4), round(float(memRate),4), " ".join(params), filen))
+   rate = float(params[0].split("=")[1])
+   results.append((rate, round(float(perform),4), round(float(memRate),4), " ".join(params), filen))
 results = sorted(results, reverse=True)
+lastR = None
 for r in results:
+   if lastR is not None and lastR[0] != r[0]:
+      print("-----------")
    print("\t".join([str(x) for x in r]))
-
+   lastR = r
