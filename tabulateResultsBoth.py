fb255f74fe83043257bfc8e007de89e490256ab3
Michael Hahn <mhahn29@gmail.com>
2020-06-29 01:38:43 -0700

    code

--- tabulateResultsBoth.py
+++ tabulateResultsBoth.py
@@ -24,6 +24,7 @@ if True:
       load_from_lm = [x.split("=")[1] for x in params if x.startswith("load_from_lm")][0]
       rate = float([x.split("=")[1] for x in params if x.startswith("RATE_WEIGHT")][0]) #params[0].split("=")[1])
       deletion_rate = float([x.split("=")[1] for x in params if x.startswith("deletion_rate")][0]) #params[0].split("=")[1])
+      predictability_weight = float(([x.split("=")[1] for x in params if x.startswith("predictability_weight")]+[0.5])[0]) #params[0].split("=")[1])
 
       params = [x for x in params if x.split("=")[0] in PARAMS]
       params_here = dict([x.split("=") for x in params])
@@ -38,15 +39,15 @@ if True:
       version, filenum = (lambda x:(filen[:x], filen[x+1:]))(filen.rfind("_"))
       script = version
       version = version[version.index("_12")+3:]
-      results.append((deletion_rate, round(predictionLoss,4), round(reconstructionLoss, 4), " ".join(params), version, filenum, load_from_lm))
-      results2.append((deletion_rate, predictionLoss, reconstructionLoss, " ".join(params), version, filenum, load_from_lm) + tuple([params_here.get(x, "NA") for x in PARAMS]))
+      results.append((deletion_rate, predictability_weight, round(predictionLoss,4), round(reconstructionLoss, 4), " ".join(params), version, filenum, load_from_lm))
+      results2.append((deletion_rate, predictability_weight, predictionLoss, reconstructionLoss, " ".join(params), version, filenum, load_from_lm) + tuple([params_here.get(x, "NA") for x in PARAMS]))
 
-   results = sorted(results, reverse=True)
-   results2 = sorted(results2, reverse=True)
+   results = sorted(results, reverse=True, key=lambda x:(x[0], x[1] * x[2] + (1-x[1]) * x[3]))
+   results2 = sorted(results2, reverse=True, key=lambda x:(x[0], x[1] * x[2] + (1-x[1]) * x[3]))
 
    lastR = None
    with open("lm_noise/tableSearchResults.tsv", "w") as outFile:
-     print("\t".join(["deletion_rate", "predictionLoss", "prediction_loss", "params", "version", "filenum", "load_from_lm"] + PARAMS), file=outFile)
+     print("\t".join(["deletion_rate", "pred_weight", "predictionLoss", "prediction_loss", "params", "version", "filenum", "load_from_lm"] + PARAMS), file=outFile)
      for r in results:
       if lastR is not None and lastR[0] != r[0]:
          print("-----------")
