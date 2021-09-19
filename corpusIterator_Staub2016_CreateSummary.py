a05539792742eb326df8f7a42c61be7dd566fbc9
Michael Hahn <mhahn29@gmail.com>
2021-09-18 18:43:22 -0700

    code

--- corpusIterator_Staub2016_CreateSummary.py
+++ corpusIterator_Staub2016_CreateSummary.py
@@ -1,10 +1,12 @@
 from paths import WIKIPEDIA_HOME
 import random
- 
+position = 0 
 
 itos_labels = []
 stoi_labels = {}
+lastItemID = None
 
+itemsDone = set()
 # open file
 with open("/u/scr/mhahn/STIMULI/Staub_2016.tsv", "w") as outFile:
   print("\t".join(["Sentence", "Item", "Condition", "Region", "Word", "NumInSent"]), file=outFile)
@@ -19,7 +21,18 @@ with open("/u/scr/mhahn/STIMULI/Staub_2016.tsv", "w") as outFile:
   chunk = []
   chunk_line_numbers = []
   for linenum, line in enumerate(text):
-    round_, item, condition, position, word, region = line #word = line[header['Word']].split("_")
-    print("\t".join([item+"_"+condition+"_"+round_, item, condition, region+("_"+str(position) if region == "PostVerb" else ""), word, str(position)]), file=outFile)
+    round_, item, condition, pos_, word, region = line #word = line[header['Word']].split("_")
+    itemID = item+"_"+condition+"_"+round_
+    if itemID != lastItemID:
+        position = 0
+        if item+"_"+condition in itemsDone:
+           continue
+        else:
+           itemsDone.add(item+"_"+condition)
+        assert int(pos_) == 0, line
+    lastItemID = itemID
+    for q, word_ in enumerate(word.split("_")):
+       print("\t".join([item+"_"+condition+"_"+round_, item, condition, region+("_"+str(position) if region == "PostVerb" else "")+("_"+str(q) if q  > 0 else ""), word_, str(position)]), file=outFile)
+       position += 1
 
 
